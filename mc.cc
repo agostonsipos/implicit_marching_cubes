061b154e2c9068716de0f2ab3aef566dbb53e24c
Ágoston Sipos <sipos.agoston@iit.bme.hu>
2019-11-19 10:13:17 +0100

    optimizing data structures

--- mc.cc
+++ mc.cc
@@ -1,7 +1,6 @@
 #include "mc.h"
 
 #include <cmath>
-#include <map>
 #include <vector>
 
 /*
@@ -38,27 +37,27 @@ namespace IMC {
 	{
 		Geometry::TriMesh mesh;
 		Geometry::PointVector points;
-		std::map<std::tuple<int,int,int>, std::vector<int>> cells;
+		auto ind3d1 = [res](int x, int y, int z) { return x * res[1] * res[2] + y * res[2] + z; };
+		auto ind3d2 = [res](int x, int y, int z) { return x * (res[1] + 1) * (res[2] + 1) + y * (res[2] + 1) + z; };
+
+		std::vector<std::vector<int>> cells(res[0] * res[1] * res[2]);
 		int index = 0;
 		double cellX = (box[1][0] - box[0][0]) / res[0];
 		double cellY = (box[1][1] - box[0][1]) / res[1];
 		double cellZ = (box[1][2] - box[0][2]) / res[2];
+		double tol = std::sqrt(cellX * cellX + cellY * cellY + cellZ * cellZ) / 1000.0;
 
-		std::vector<std::vector<std::vector<Geometry::Vector3D>>> cellCorners(res[0]+1);
-		std::vector<std::vector<std::vector<double>>> cellValues(res[0]+1);
+		std::vector<Geometry::Vector3D> cellCorners((res[0] + 1) * (res[1] + 1) * (res[2] + 1));
+		std::vector<double> cellValues((res[0] + 1) * (res[1] + 1) * (res[2] + 1));
 		for (int i = 0; i <= res[0]; ++i) {
-			cellCorners[i].resize(res[1] + 1);
-			cellValues[i].resize(res[1] + 1);
 			for (int j = 0; j <= res[1]; ++j) {
-				cellCorners[i][j].resize(res[2] + 1);
-				cellValues[i][j].resize(res[2] + 1);
 				for (int k = 0; k <= res[2]; ++k) {
-					cellCorners[i][j][k] = Geometry::Vector3D(
+					cellCorners[ind3d2(i, j, k)] = Geometry::Vector3D(
 						box[0][0] + cellX * i,
 						box[0][1] + cellY * j,
 						box[0][2] + cellZ * k
 					);
-					cellValues[i][j][k] = scalarFunc(cellCorners[i][j][k]);
+					cellValues[ind3d2(i, j, k)] = scalarFunc(cellCorners[ind3d2(i, j, k)]);
 				}
 			}
 		}
@@ -72,8 +71,8 @@ namespace IMC {
 					int cubeindex = 0;
 					for (int c = 0; c < 8; ++c) {
 						int x = i + Tables::cornerPositions[c][0], y = j + Tables::cornerPositions[c][1], z = k + Tables::cornerPositions[c][2];
-						corners[c] = cellCorners[x][y][z];
-						values[c] = cellValues[x][y][z];
+						corners[c] = cellCorners[ind3d2(x, y, z)];
+						values[c] = cellValues[ind3d2(x, y, z)];
 						if (values[c] < isolevel) cubeindex |= (1 << c);
 					}
 					std::array<Geometry::Vector3D, 12> vertlist;
@@ -86,7 +85,6 @@ namespace IMC {
 						}
 					}
 
-					double tol = std::sqrt(cellX * cellX + cellY * cellY + cellZ * cellZ) / 1000.0;
 					for (int e = 0; Tables::triTable[cubeindex][e] != -1; e += 3) {
 						std::array<Geometry::Vector3D, 3> vertices = {
 							vertlist[Tables::triTable[cubeindex][e]],
@@ -97,12 +95,14 @@ namespace IMC {
 						for (int t = 0; t < 3; ++t) {
 							bool foundPoint = false;
 							for (int x = 0; x < 8 && !foundPoint; ++x) {
-								std::vector<int>& list = cells[{i - ((x >> 2) & 1), j - ((x >> 1) & 1), k - (x & 1)}];
+								int cellInd = ind3d1(i - ((x >> 2) & 1), j - ((x >> 1) & 1), k - (x & 1));
+								if (i - ((x >> 2) & 1) < 0 || j - ((x >> 1) & 1) < 0 || k - (x & 1) < 0) continue;
+								std::vector<int>& list = cells[cellInd];
 								for (auto it = list.begin(); it != list.end(); ++it)
 								{
 									if ((points[*it] - vertices[t]).norm() < tol) {
 										indices[t] = *it;
-										cells[{i,j,k}].push_back(indices[t]);
+										cells[ind3d1(i, j, k)].push_back(indices[t]);
 										foundPoint = true;
 										break;
 									}
@@ -111,7 +111,7 @@ namespace IMC {
 							if (!foundPoint) {
 								points.push_back(vertices[t]);
 								indices[t] = index++;
-								cells[{i,j,k}].push_back(indices[t]);
+								cells[ind3d1(i, j, k)].push_back(indices[t]);
 							}
 						}
 						mesh.addTriangle(indices[0], indices[1], indices[2]);
